9f94160d15d14e332bc3b2679584f4c95567c166
Andrej Kolic <andrej.kolic@gmail.com>
2017-04-02 17:05:20 +0200

    initial commit

---
+++ minimal/index.js
@@ -0,0 +1,41 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+function jsonRequest(url) {
+  return fetch(url)
+    .then(function (response) {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      return response.json();
+    })
+}
+
+var userId = 1;
+var user = null;
+var posts = null;
+
+Promise.all([
+  jsonRequest('http://jsonplaceholder.typicode.com/users/' + userId)
+    .then(function (json) {
+      console.log('user fetched');
+      user = json;
+    }),
+  jsonRequest('http://jsonplaceholder.typicode.com/posts?userId=' + userId)
+    .then(function (json) {
+      console.log('posts fetched');
+      posts = json;
+    })
+]).then(function () {
+  console.info('data loaded');
+  user.posts = posts.sort(function (a, b) {
+    return a.id < b.id ? 1 : -1
+  });
+  posts.forEach(function (element) {
+    element.user = user;
+  });
+  console.log('latest post for user ' + user.name + ' is titled ' + user.posts[0].title)
+}).catch(function (err) {
+  console.error(err);
+});
+
